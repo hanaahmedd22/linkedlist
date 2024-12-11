72b71992f34612cd69e5d37393367c00955a71e9
Hana Ahmed <102739616+hanaahmedd22@users.noreply.github.com>
2024-12-11 15:22:04 +0200

    format spacing update

--- linkedList.h
+++ linkedList.h
@@ -5,90 +5,86 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 #include "Node.h"
-int counter=0;
+int counter = 0;
 using namespace std;
-class LinkedList
-{
 
-    Node * head;
-    Node * tail;
+class LinkedList {
+    Node *head;
+    Node *tail;
+
 public:
     LinkedList() {
-        head=tail=NULL;
+        head = tail = NULL;
     }
-    void addNode(int _data){
-        //Create Node
-        Node *newNode=new Node(_data); // Nodedata=data , next,prev=null;
-        if(head == NULL){
-            head=tail=newNode;
-            return ;
-        }
-        tail->next=newNode;
-        newNode->prev=tail;
-        tail=newNode;
 
-    }
-    void display(){
-        Node *current=head;
-        if(current == NULL){
-            cout<<"Linked List Empty \n";
+    void addNode(int _data) {
+        //Create Node
+        Node *newNode = new Node(_data); // Nodedata=data , next,prev=null;
+        if (head == NULL) {
+            head = tail = newNode;
             return;
         }
-        while(current != NULL ){
-            cout<<current->data<<"\t";
-            current=current->next;
-        }
-
-
-
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
     }
+
+    void display() {
+        Node *current = head;
+        if (current == NULL) {
+            cout << "Linked List Empty \n";
+            return;
+        }
+        while (current != NULL) {
+            cout << current->data << "\t";
+            current = current->next;
+        }
+    }
+
     int getcount() {
-        Node *current=head;
-        if(current == NULL){
-            cout<<"Linked List Empty \n";
+        Node *current = head;
+        if (current == NULL) {
+            cout << "Linked List Empty \n";
             return 0;
         }
-        while(current != NULL ){
+        while (current != NULL) {
             counter++;
-            current=current->next;
+            current = current->next;
         }
         return counter;
     }
 
-    void Remove(int _data){
-        Node * node=GetNode(_data);
-        if(node==NULL){
-            cout<<"Not Found "<<endl;
-            return ;
+    void Remove(int _data) {
+        Node *node = GetNode(_data);
+        if (node == NULL) {
+            cout << "Not Found " << endl;
+            return;
         }
-        if(head == node){
-            if(head == tail){
-                head=tail=NULL;
+        if (head == node) {
+            if (head == tail) {
+                head = tail = NULL;
+            } else {
+                head = head->next;
+                head->prev = NULL;
             }
-            else{
-                head=head->next;
-                head->prev=NULL;
-            }
-        }
-        else if(tail == node){
-            tail=tail->prev;
-            tail->next=NULL;
-        }
-        else{
-            Node * A= node->prev;
-            Node * B= node->next;
-            A->next=B;
-            B->prev=A;
-
+        } else if (tail == node) {
+            tail = tail->prev;
+            tail->next = NULL;
+        } else {
+            Node *A = node->prev;
+            Node *B = node->next;
+            A->next = B;
+            B->prev = A;
         }
         delete node;
     }
-    void insertbefore(int _data,int beforenodedata) {
-        Node * newNode=new Node(_data);
-        Node* bNode=GetNode(beforenodedata);
+
+    void insertbefore(int _data, int beforenodedata) {
+        Node *newNode = new Node(_data);
+        Node *bNode = GetNode(beforenodedata);
         if (bNode != NULL) {
             if (head == bNode) {
-                head->prev = newNode;//head kan prev b null b2a byshawr 3la new node
+                head->prev = newNode; //head kan prev b null b2a byshawr 3la new node
                 newNode->next = head;
                 head = newNode;
             } else {
@@ -98,73 +94,70 @@ public:
                 newNode->prev = A;
                 A->next = newNode;
             }
-            cout<< "successful insertion "<<endl;
-            return ;
+            cout << "successful insertion " << endl;
+            return;
         } else {
             cout << "Node Not Found " << endl;
             return;
         }
     }
-    void insertafter(int _data,int afternodedata) {
-        Node * newNode=new Node(_data);
-        Node* aNode=GetNode(afternodedata);
-        if (aNode==NULL) {
-            cout<<"Node Not Found "<<endl;
-            return ;
-        }
-        else if(aNode==tail) {
-                aNode->next=newNode;
-                newNode->prev=aNode;
-                tail=newNode;
-            }
-        else {
-            Node * B=aNode->next;
-            aNode->next=newNode;
-            newNode->prev=aNode;
-            newNode->next=B;
-            B->prev=newNode;
 
+    void insertafter(int _data, int afternodedata) {
+        Node *newNode = new Node(_data);
+        Node *aNode = GetNode(afternodedata);
+        if (aNode == NULL) {
+            cout << "Node Not Found " << endl;
+            return;
+        } else if (aNode == tail) {
+            aNode->next = newNode;
+            newNode->prev = aNode;
+            tail = newNode;
+        } else {
+            Node *B = aNode->next;
+            aNode->next = newNode;
+            newNode->prev = aNode;
+            newNode->next = B;
+            B->prev = newNode;
         }
-        cout<< "successful insertion "<<endl;
-        return ;
+        cout << "successful insertion " << endl;
+        return;
     }
+
     int getDatabyIndex(int index) {
-        int i=0;
-        Node *current=head;
-        if(current == NULL){
-            cout<<"Linked List Empty \n";
+        int i = 0;
+        Node *current = head;
+        if (current == NULL) {
+            cout << "Linked List Empty \n";
             return 0;
         }
-        while(current != NULL ){
-           if(i==index) {
-               return current->data;
-           }
+        while (current != NULL) {
+            if (i == index) {
+                return current->data;
+            }
             i++;
-            current=current->next;
+            current = current->next;
         }
-        cout<<"index Not Found "<<endl;
+        cout << "index Not Found " << endl;
         return 0;
-
-
-
     }
-    ~LinkedList() {}
+
+    ~LinkedList() {
+    }
 
 protected:
 
 private:
     //Helper Function
-    Node * GetNode(int _data){
-        Node *current=head;
-        while(current != NULL ){
-            if(current->data == _data){
+    Node *GetNode(int _data) {
+        Node *current = head;
+        while (current != NULL) {
+            if (current->data == _data) {
                 return current;
             }
-            current=current->next;
+            current = current->next;
         }
         return NULL;
     }
 };
 
 #endif // LINKEDLIST_H
-
--- main.cpp
+++ main.cpp
@@ -4,33 +4,32 @@
 using namespace std;
 
 int main() {
+    LinkedList l;
+    LinkedList *p = new LinkedList();
 
-        LinkedList l;
-        LinkedList *p=new LinkedList();
+    l.addNode(5);
+    l.addNode(10);
+    cout << "number of nodes: " << l.getcount() << endl;
 
-        l.addNode(5);
-        l.addNode(10);
-        cout<<"number of nodes: "<<l.getcount()<<endl;
-
-        l.addNode(15);
-    int index=0;
-    cout<< "index of node "<<index<<" has data  " <<l.getDatabyIndex(index)<<endl;
-        l.display();
-        cout<<endl;
-        l.getDatabyIndex(5);
-       // l.Remove(15);
-        //l.Remove(10);
-       // l.display();
-        //l.Remove(5);
-    l.insertafter(101,15);
-    l.insertafter(102,10);
-    l.insertafter(103,5);//5 103 10 102 15 101
+    l.addNode(15);
+    int index = 0;
+    cout << "index of node " << index << " has data  " << l.getDatabyIndex(index) << endl;
     l.display();
-    cout<<endl;
-    l.insertbefore(200,5);
-    l.insertbefore(201,10);
-    l.insertbefore(202,101);
-    cout<<endl;
+    cout << endl;
+    l.getDatabyIndex(5);
+    // l.Remove(15);
+    //l.Remove(10);
+    // l.display();
+    //l.Remove(5);
+    l.insertafter(101, 15);
+    l.insertafter(102, 10);
+    l.insertafter(103, 5); //5 103 10 102 15 101
+    l.display();
+    cout << endl;
+    l.insertbefore(200, 5);
+    l.insertbefore(201, 10);
+    l.insertbefore(202, 101);
+    cout << endl;
     l.display();
 
 
